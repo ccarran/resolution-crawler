b0a6f15cd76bd0cd8c769876786151121065fb88
ccarran <52898556+ccarran@users.noreply.github.com>
2024-01-12 19:43:45 -0500

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,90 @@
+Contents:
+  1. Libraries
+  2. Internal Keyword setup
+  3. Auto Whatsapp Message setup
+  4. Running the script
+
+-------------------------------------------------------------------------------------------------------------
+
+1. Libraries Used:
+  - bs4 (BeautifulSoup)
+  - requests_html
+  - pywhatkit
+  - os
+  - sys
+
+1.1 Installing necessary libraries:
+
+While some libraries are included in Python by default, we'll need to manually install some of the other libs 
+used in the script. To install them, we'll need to go to the CMD, PowerShell, or other preferred console, and
+type the following commands.
+  - bs4: pip install beautifulsoup4
+  - requests_html: pip install requests-html
+  - pywhatkit: pip install pywhatkit
+
+-------------------------------------------------------------------------------------------------------------
+
+Setups:
+
+This scripts makes use of constants, located at the start of the code, which are used in multiple of its
+features. To properly make use of those features, you'll have to setup those constants.
+
+-------------------------------------------------------------------------------------------------------------
+
+2. Internal Keyword setup:
+
+When using the keyword mode, the script will look for articles containing certain keywords. These keywords
+can come from an external file, or from the list KEYWORDS located at the top of the script. To set up your
+keywords to search for, fill in this list with string keywords as such:
+
+KEYWORDS = ["word1", "word2", "word3"]
+
+It's also possible to search for sub-strings in the articles, but bear in mind that the script will not look
+for the elements of the substring separatedly, and will only flag exact matches for the substring. E.g.:
+
+KEYWORDS = ["word1", "sub string 1", "word2"]
+
+-------------------------------------------------------------------------------------------------------------
+
+3. Auto Whatsapp Message setup:
+
+When using the auto message mode, the script will send a whatsapp message to the specified group at an
+established time. This information can be set at the WPP_GROUP_ID, AUTO_MSG_HOUR, and AUTO_MSG_MIN constants
+located at the top of the script. WPP_GROUP_ID should be filled with a string group id, which can be obtained
+from a whatsapp group invitation link (e.g. https://chat.whatsapp.com/etvyBn4FS4Y3OJSA793rGL, where the ID is
+the final group of characters); and AUTO_MSG_HOUR and AUTO_MSG_MIN are integers for the hour and minute where
+the message will be sent (hour must be set in 24 hour format). E.g.:
+
+WPP_GROUP_ID = etvyBn4FS4Y3OJSA793rGL
+AUTO_MSG_HOUR = 17
+AUTO_MSG_MIN = 30
+
+3.1 Other considerations:
+
+Using the auto message mode requires an active whatsapp session already open in the default web browser, if
+there isn't a previously active session, running the script in this mode will open a log-in prompt.
+
+IMPORTANT: The auto message mode is working, but rough. When the hour set for the script to send the message,
+it will take the user system's input to type in and send the message. Therefore, to avoid issues with this
+feature, it is recommended to not use the system while the auto message is being typed.
+
+Also, typing the message is done character-by-character, which means that longer messages can take a long span
+of time to be typed and sent. 
+
+-------------------------------------------------------------------------------------------------------------
+
+4. Running the script:
+
+From your console, navigate to the location of the script. Then, run the following command:
+
+	python crawler.py [-k] [-f ('PATH_TO_FILE' 'SEPARATOR_CHARACTER')] [-w]
+
+	Optional flags:
+	-k: enables the keyword mode. If this flag is set alone, the internal list of keywords is used
+	-f: takes keywords from an external file. If this flag is set the next two arguments must be:
+		* "PATH_TO_FILE": A string path to the file from which to extract the keywords
+		* "SEPARATOR_CHARACTER": A string character used in the separator file to separate keywords.
+					 The new line character ('\n') is assumed by default.
+	-w: enables the auto message mode.
+
+	Output: A .txt file with the relevant articles from the website.
---
+++ crawler.py
@@ -0,0 +1,205 @@
+from bs4 import BeautifulSoup
+from requests_html import HTMLSession
+import pywhatkit as pwk
+import sys, os
+
+KEYWORDS = []                                   # Fill this list with string keywords, separated by commas
+WPP_GROUP_ID = None                             # Fill here for whatsapp group id to send results
+AUTO_MSG_HOUR = None                            # Fill here for auto message hour (24 hours format)
+AUTO_MSG_MIN = None                             # Fill here for auto message minute
+
+def getArticles():
+    """
+    Requests articles from the specified website, returns a list with all articles found
+
+    Input: none
+    Output: List of html segments with relevant articles
+    """
+    url = "https://diariooficial.elperuano.pe/Normas"
+
+    # Makes a session to render js scripts
+    session = HTMLSession()
+    r = session.get(url)
+    r.html.render(sleep=5)  # Sleep is set to 5 to allow page to render, the value of sleep could be changed
+
+    soup = BeautifulSoup(r.html.html, 'html.parser')
+    articles = soup.find_all('article', class_=['edicionesoficiales_articulos', 'edicionesoficiales_articulos_dig'])
+
+    return articles
+
+def getInfo(article):
+    """
+    Takes an article previously crawled, and generates a dictionary with keys containing
+    relevant information (title, resolution, date, description, link)
+
+    Input:
+        article: a single article from the ones previously crawled
+    
+    Output: a dictionary containing relevant information from the article
+    """
+    articleInfo = {}
+
+    articleInfo["title"] = article.find("h4").text
+    articleInfo["resolucion"] = article.find("h5").find('a').text
+    articleInfo["date"] = article.find("b").text.strip()
+
+    # Checks for the 'extraordinaria' tag
+    extraordinaria = article.find("strong", class_="extraordinaria")
+    if extraordinaria:
+        articleInfo["date"] += ' ' + extraordinaria.text
+
+    articleInfo["description"] = article.find_all("p")[1].text
+    articleInfo["link"] = article.find("h5").find('a').get('href')
+
+    return articleInfo
+
+def writeToFile(articleList):
+    """
+    Writes the contents from an articleList containing dictionaries with article information,
+    to a text file 'results.txt'
+
+    Input:
+        articleList: a list of dictionaries containing information on articles
+
+    Output: none
+    """
+    file = open('results.txt', 'w')
+
+    for article in articleList:
+        file.write(article['title'] + '\n')
+        file.write(article['resolucion'] + '\n')
+        file.write(article['date'] + '\n')
+        file.write('\n' + article['description'] + '\n')
+        file.write('\n' + article['link'] + '\n')
+        
+        if article != articleList[-1]:
+            file.write('\n' + '-'*50 + '\n'*2)
+
+    file.close()
+
+def extractKeyArticles(articleList, keywordList):
+    """
+    Takes a list of dictionaries with article information and returns a list with
+    only the articles with a description that contains one of the specified keywords
+
+    Input:
+        articleList: a list of dictionaries containing information on article
+        keywordList: a list of string keywords
+
+    Output: a filtered list of dictionaries that contain any of the keyword
+    """
+    newArticleList = []
+
+    for article in articleList:
+        for keyword in keywordList:
+            if keyword.lower() in article['description'].lower():
+                newArticleList.append(article)
+                break
+
+    return newArticleList
+
+def getKeywordsFromFile(pathToFile, separator):
+    """
+    Gets the keyword from an external text file
+
+    Input:
+        pathToFile: a string path to the text file containing the keywords
+        separator: a string separator used in the keyword file
+
+    Output: a list of string keywords
+    """
+    keywordList = []
+
+    file = open(pathToFile, 'r')
+
+    for line in file:
+        for keyword in line.split(separator):
+            if keyword:
+                keywordList.append(keyword.strip())
+
+    return keywordList
+
+def whatsappResults(articleList):
+    """
+    Sends a whatsapp message from the 
+
+    Input: 
+        articleList: a list of dictionaries containing information on article
+
+    Output: none
+    """
+    results = ''
+
+    for article in articleList:
+        results += article['title'] + '\n'
+        results += article['resolucion'] + '\n'
+        results += article['date'] + '\n'
+        results += '\n' + article['description'] + '\n'
+        results += '\n' + article['link'] + '\n'
+        
+        if article != articleList[-1]:
+            results += '\n' + '-'*50 + '\n'*2
+
+    pwk.sendwhatmsg_to_group(WPP_GROUP_ID, results, AUTO_MSG_HOUR, AUTO_MSG_MIN, 30)
+
+def main():
+    keywordMode = False
+    externalKeywordList = False
+    autoMsgMode = False
+
+    # CLI
+    if len(sys.argv) < 1:
+        raise Exception("Incorrect argument number. Usage: python crawler.py [-k] [-f ('PATH_TO_FILE' 'SEPARATOR_CHARACTER')] [-w]")
+    
+    if "-k" in sys.argv:
+        keywordMode = True
+
+    if '-f' in sys.argv:
+        externalKeywordList = True
+
+        try:
+            keywordPath = sys.argv[sys.argv.index('-f') + 1]
+            if not os.path.exists(keywordPath):
+                raise Exception("Invalid path. Make sure to specify a valid path after the -f flag")
+        except:
+            raise Exception("Path to keyword file not provided. Usage: python crawler.py [-k] [-f ('PATH_TO_FILE' 'SEPARATOR_CHARACTER')] [-w]")
+        
+        try:
+            keywordFileSeparator = sys.argv[sys.argv.index('-f') + 2]
+            if len(keywordFileSeparator) > 1:
+                raise Exception("Invalid separator. Expected single character.")
+        except:
+            print("Separator not specified. Assuming new line character as separator...")
+            keywordFileSeparator = '\n'
+
+    if '-w' in sys.argv:
+        autoMsgMode = True
+
+    # Get articles from website
+    articles = getArticles()
+    articleList = []
+
+    # Make a list with the info from the articles
+    for article in articles:
+        articleList.append(getInfo(article))
+
+    # Checks whether to use kw mode or extKwList mode
+    if keywordMode or externalKeywordList:
+        if externalKeywordList:
+            keywordList = getKeywordsFromFile(keywordPath, keywordFileSeparator)
+        elif len(KEYWORDS) > 0:
+            keywordList = KEYWORDS
+        else:
+            print("Keyword list empty. All articles extracted.")
+
+        articleList = extractKeyArticles(articleList, keywordList)
+
+    # Writes results to a file
+    writeToFile(articleList)
+    
+    # Automates a whatsapp message with the results
+    if autoMsgMode:
+        whatsappResults(articleList)
+
+if __name__ == "__main__":
+    main()
